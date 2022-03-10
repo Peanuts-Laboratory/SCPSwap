f57bf937d2db6f7a5931cb4cd08cdffe05606f0f
Esser <upwordstudent@gmail.com>
2022-03-10 12:40:26 -0600

    Actually set a proper version :trollface:
    I am retarded

--- SCPSwap/SCPSwap.cs
+++ SCPSwap/SCPSwap.cs
@@ -9,7 +9,7 @@ namespace ScpSwap
 		public EventHandlers Handler { get; private set; }
 		public override string Name => nameof(ScpSwap);
 		public override string Author => "Originally written by Cyanox, modifications by KoukoCocoa & Thomasjosif, Maintained by EsserGaming";
-		public override Version Version { get; } = new Version(1, 0, 4);
+		public override Version Version { get; } = new Version(1, 3, 3);
 		public override Version RequiredExiledVersion { get; } = new Version(5, 0, 0);
 
 		public ScpSwap() { }
